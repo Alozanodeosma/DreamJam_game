307a82aa7cf0c28a56aedb62ec841d2ef7b41b4a
Alvaro Lozano de Osma <alvarolozano16@gmail.com>
2024-04-13 15:52:50 +0200

    terreno + personaje

--- Assets/Scripts/UI/SliderMovement.cs
+++ Assets/Scripts/UI/SliderMovement.cs
@@ -26,19 +26,19 @@ public class SliderMovement : MonoBehaviour, IPointerDownHandler, IDragHandler
     }
     void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
     {
-        if (lastPos==currentPos) { 
+        //if (lastPos==currentPos) { 
             screenPosition = camera.WorldToScreenPoint(transform.position); //transforma la posición del objeto de la posición en el mundo virtual a una posición en la pantalla
             Vector3 vec3 = Input.mousePosition - screenPosition;
             angleOffset = (Mathf.Atan2(transform.right.y, transform.right.x) - Mathf.Atan2(vec3.y, vec3.x)) * Mathf.Rad2Deg;//calcula el ángulo entre el objeto y el ratón
-        }
+        //}
     }
     void IDragHandler.OnDrag(UnityEngine.EventSystems.PointerEventData eventData)
     {
-        if (lastPos == currentPos)
-        {
+        //if (lastPos == currentPos)
+        //{
             Vector3 vec3 = Input.mousePosition - screenPosition;
             float angle = Mathf.Atan2(vec3.y, vec3.x) * Mathf.Rad2Deg;
             transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, angle + angleOffset);
-        }
+        //}
     }
 }
