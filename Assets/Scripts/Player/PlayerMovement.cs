8adef458c3185f8c532f8d345ad7c15fed973855
Alvaro Lozano de Osma <alvarolozano16@gmail.com>
2024-04-13 23:15:35 +0200

    ayuda

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -13,15 +13,23 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private GameObject sliderWalk;
     private Rigidbody rb;
 
-
+    float rotationSpeed = 200f; // Velocidad de rotación en grados por segundo
     public float speed = 5f; // Velocidad del jugador
     public Vector3 moveDirection; // Dirección del movimiento
+    bool left = false;
+    bool right = false;
     void Start()
     {
         rb = player.GetComponent<Rigidbody>();
 
     }
+    public float sensitivity = 0.5f; // Adjust this to control the sensitivity of rotation
+    public float maxRotation = 180f; // Maximum rotation in degrees
+    public float rotationSpeedMultiplier = 1f; // Adjust this to control the rotation speed multiplier
+    private float draggedRotationTotal;
 
+    private Vector3 lastMousePosition;
+    private float accumulatedRotation;
     // Update is called once per frame
     void Update()
     {
@@ -35,10 +43,34 @@ public class PlayerMovement : MonoBehaviour
             rb.useGravity = true;          
         }
 
+        if (Input.GetMouseButton(0))
+        {
+            // Calculate the difference in mouse position since the last frame
+            Vector3 currentMousePosition = Input.mousePosition;
+            Vector3 mouseDelta = currentMousePosition - lastMousePosition;
 
+            // Calculate rotation speed based on mouse movement and sensitivity
+            float rotationSpeed = -mouseDelta.x * rotationSpeedMultiplier * Time.deltaTime;
 
+            // Apply rotation to the dragged object
+            sliderRotation.transform.Rotate(Vector3.up, rotationSpeed);
 
+            // Update the total rotation of the dragged object
+            draggedRotationTotal += rotationSpeed;
+
+            // Calculate the rotation for the stationary object based on the total rotation of the dragged object
+            float stationaryRotation = Mathf.Lerp(90f, -90f, Mathf.PingPong(draggedRotationTotal / 360f, 1f));
+
+            // Apply the rotation to the stationary object
+            transform.rotation = Quaternion.Euler(0f, stationaryRotation, 0f);
+        }
+
+        // Store the current mouse position for the next frame
+        lastMousePosition = Input.mousePosition;
     }
+
+
+
 
     void FixedUpdate()
     {
@@ -55,7 +87,20 @@ public class PlayerMovement : MonoBehaviour
         //rota al juagdor
         if (sliderRotation.GetComponent<RectTransform>().eulerAngles.z != transform.rotation.eulerAngles.y)
         {
-            Rotate((int)sliderRotation.GetComponent<Transform>().eulerAngles.z);
+            //make the max rotation speed 1.5f
+            //rotate left from 0f speed to 1.5f speed while left is true
+            if (left)
+            {
+                transform.Rotate(Vector3.up * 1.5f);
+            }
+            else if(right)
+            {
+                transform.Rotate(Vector3.down * 1.5f);
+            }
+            else
+            {
+                transform.Rotate(Vector3.zero);
+            }
         }
     }
     public void Move()
@@ -64,11 +109,16 @@ public class PlayerMovement : MonoBehaviour
         GetComponent<Rigidbody>().MovePosition(transform.position + moveDirection * speed * Time.deltaTime);
     }
     public void Rotate(int y)
-    {
+    {/*
         Vector3 rotation = new Vector3(0.0f, y, 0.0f);
 
         transform.rotation = Quaternion.Euler(rotation);
+        */
 
+
+            float rotationAmount = rotationSpeed * Time.deltaTime;
+            transform.Rotate(0, -rotationAmount, 0);
+        
     }
 
 }
--- Assets/Scripts/UI/SliderMovement.cs
+++ Assets/Scripts/UI/SliderMovement.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class SliderMovement : MonoBehaviour, IPointerDownHandler, IDragHandler
+public class SliderMovement : MonoBehaviour//, IPointerDownHandler, IDragHandler
 {
     private Camera camera;
     private Vector3 screenPosition;
@@ -15,30 +15,42 @@ public class SliderMovement : MonoBehaviour, IPointerDownHandler, IDragHandler
     void Start()
     {
         camera = Camera.main;
-        
+
     }
-    private Vector3 lastPos;
-    private Vector3 currentPos;
+    private float lastPosSlider=0;
+    private float currentPosSlider=0;
+    public bool left=false;
+
+
+    
+    
+    public bool right=false;
+    private void Update()
+    {
+        lastPosSlider = currentPosSlider;
+        currentPosSlider = this.transform.rotation.z;
+
+    }
     private void FixedUpdate()
     {
-        lastPos=currentPos;
-        currentPos=player.transform.position;
-    }
-    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
-    {
-            angleOffset = 0;
-            screenPosition = camera.WorldToScreenPoint(transform.position); //transforma la posición del objeto de la posición en el mundo virtual a una posición en la pantalla
-            Vector3 vec3 = Input.mousePosition - screenPosition;
-            angleOffset = (Mathf.Atan2(transform.right.y, transform.right.x) - Mathf.Atan2(vec3.y, vec3.x)) * Mathf.Rad2Deg;//calcula el ángulo entre el objeto y el ratón
-        
-    }
-    void IDragHandler.OnDrag(UnityEngine.EventSystems.PointerEventData eventData)
-    {
 
-            Vector3 vec3 = Input.mousePosition - screenPosition;
-            float angle = Mathf.Atan2(vec3.y, vec3.x) * Mathf.Rad2Deg;
-            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, angle + angleOffset);
-
-        
     }
+    //void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    //{
+    //    angleOffset = 0;
+    //    screenPosition = camera.WorldToScreenPoint(transform.position); //transforma la posición del objeto de la posición en el mundo virtual a una posición en la pantalla
+    //    Vector3 vec3 = Input.mousePosition - screenPosition;
+    //    angleOffset = (Mathf.Atan2(transform.right.y, transform.right.x) - Mathf.Atan2(vec3.y, vec3.x)) * Mathf.Rad2Deg;//calcula el ángulo entre el objeto y el ratón
+
+    //}
+    //void IDragHandler.OnDrag(UnityEngine.EventSystems.PointerEventData eventData)
+    //{
+
+    //    Vector3 vec3 = Input.mousePosition - screenPosition;
+    //    float angle = Mathf.Atan2(vec3.y, vec3.x) * Mathf.Rad2Deg;
+    //    //rotate the slider
+    //    //transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, angle + angleOffset);
+    //    Quaternion targetRotation = Quaternion.LookRotation(transform.forward, Vector3.up) * Quaternion.Euler(0, 0, angle + angleOffset);
+    //    transform.rotation = targetRotation;
+    //}
 }
