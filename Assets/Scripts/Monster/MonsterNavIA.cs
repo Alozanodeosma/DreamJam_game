0b1b87c483e75db59be5e870c783f2767d80bad9
DanielFure4 <64164594+ddelinosgarciaMM2021@users.noreply.github.com>
2025-03-14 23:37:53 +0900

    Pruebas wiwi

--- Assets/Scripts/Monster/MonsterNavIA.cs
+++ Assets/Scripts/Monster/MonsterNavIA.cs
@@ -9,18 +9,32 @@ public class MonsterNavAI : MonoBehaviour
     public GameObject player;
     private Vector3 playerPos;
     private NavMeshAgent agent;
+    private float startWiwiSpeed;
+    public float distance;
 
     // Start is called before the first frame update
     void Start()
     {
         agent= GetComponent<NavMeshAgent>();
+        startWiwiSpeed = agent.speed;
     }
 
     // Update is called once per frame
     void Update()
     {
+        distance = agent.remainingDistance;
         playerPos = player.transform.position;
         agent.destination = playerPos;
+
+        print(agent.remainingDistance);
+
+        if (agent.remainingDistance > 100)
+        {
+            agent.speed = startWiwiSpeed*2;
+        } else if (agent.remainingDistance < 75)
+        {
+            agent.speed = startWiwiSpeed;
+        }
     }
 
 
