d9c6fb6876a65e2df6f77a55491a8c259f352759
Alvaro Lozano de Osma <alvarolozano16@gmail.com>
2025-03-18 15:28:05 +0100

    Revert de fure

--- Assets/Scripts/Monster/MonsterNavIA.cs
+++ Assets/Scripts/Monster/MonsterNavIA.cs
@@ -9,36 +9,31 @@ public class MonsterNavAI : MonoBehaviour
     public GameObject player;
     private Vector3 playerPos;
     private NavMeshAgent agent;
-    public float startWiwiSpeed;
-    public float wiwiSprintSpeed;
-    public float innerRange;
-    public float outerRange;
+    private float startWiwiSpeed;
     public float distance;
 
     // Start is called before the first frame update
     void Start()
     {
         agent= GetComponent<NavMeshAgent>();
-        agent.speed = startWiwiSpeed;
+        startWiwiSpeed = agent.speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        distance = agent.remainingDistance;
         playerPos = player.transform.position;
         agent.destination = playerPos;
-        distance = Vector3.Distance(playerPos,agent.transform.position);
 
+        print(agent.remainingDistance);
 
-        if (distance > outerRange)
-         {
-             agent.speed = wiwiSprintSpeed;
-            print("sprint: " + agent.speed);
-         } else if (distance < innerRange)
-         {
-             agent.speed = startWiwiSpeed;
-            print("andar: " + agent.speed);
+        if (agent.remainingDistance > 100)
+        {
+            agent.speed = startWiwiSpeed*2;
+        } else if (agent.remainingDistance < 75)
+        {
+            agent.speed = startWiwiSpeed;
         }
     }
 
